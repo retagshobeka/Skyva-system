a57be465374fd6f1ddd93d75fe367c561c0c1d4c
retagshobeka <retagshobeka19@gmail.com>
2026-03-17 01:09:13 +0300

    add flights date

--- backend/routes/flights.js
+++ backend/routes/flights.js
@@ -2,7 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Flights route working" });
+  res.json([
+    {
+      id: 1,
+      airline: "Syrian Air",
+      from: "Damascus",
+      to: "Dubai",
+      price: 350
+    },
+    {
+      id: 2,
+      airline: "Cham Wings",
+      from: "Damascus",
+      to: "Istanbul",
+      price: 280
+    }
+  ]);
 });
 
 module.exports = router;
